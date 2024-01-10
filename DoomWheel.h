d738975d887c81291560e5639654ba28b585aaf3
Krosny Kevin <kevin.krosny@protonmail.com>
2024-01-10 12:06:36 +0100

    Added an area of pit-outcome. Changed functionality of pit-outcome.

--- DoomWheel.h
+++ DoomWheel.h
@@ -7,11 +7,12 @@ class IRSDK_Handler;
 class DoomWheel
 {
 public:
-	static void RenderUI(ImFont *fonts[], IRSDK_Handler *irsdk);
+	static void RenderUI(ImFont *font, IRSDK_Handler *irsdk);
 
 private:
-    static void draw_pit_outcome(float lost_time, ImVec2 win_center, int radius, 
-        int car_radius, int car_segments, bool b_transparent, IRSDK_Handler* irsdk);
+    static void draw_pit_outcome(float lost_time, float variance, ImVec2 win_center, 
+        int radius, int car_radius, int car_segments, bool b_transparent, 
+        IRSDK_Handler* irsdk);
 
     static ImVec2 calc_center(ImVec2 pos, ImVec2 size);
     static ImVec2 calc_right_bottom_corner(ImVec2 pos, ImVec2 size);
